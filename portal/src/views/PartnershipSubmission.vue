893da1eb7e47795359f11a8924ba860143fac570
dzakirafabillah <zigdzak27@gmail.com>
2022-05-25 00:15:10 +0700

    Update PartnershipSubmission.vue

--- portal/src/views/PartnershipSubmission.vue
+++ portal/src/views/PartnershipSubmission.vue
@@ -1,8 +1,153 @@
 <template>
   <div id="agenda">
     <Header></Header>
-    <h3>FORMULIR PENGAJUAN KERJASAMA</h3>
+    <h2>FORMULIR PENGAJUAN KERJASAMA</h2>
     <div class="partnership-submission">
+        <v-container fluid >
+          <v-row>
+            <v-col cols="4">
+              <p class="font-weight-bold">Mitra Kerja Sama</p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+              <v-text-field
+                class="rounded-0 ml-4"
+                label="Mitra Kerja Sama"
+                solo
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+           <v-row>
+            <v-col cols="4">
+              <p
+              >Data PIC Mitra</p>
+            </v-col>
+          </v-row>
+
+           <v-row>
+            <v-col cols="4">
+              <p>Nama</p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+              <v-text-field
+                class="rounded-0 ml-4"
+                label="Nama"
+                solo
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+           <v-row>
+            <v-col cols="4">
+              <p>Nomor Telepon</p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+              <v-text-field
+                class="rounded-0 ml-4"
+                label="Nomor telepon"
+                solo
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+           <v-row>
+            <v-col cols="4">
+              <p>Email</p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+              <v-text-field
+                class="rounded-0 ml-4"
+                label="Email"
+                solo
+              ></v-text-field>
+            </v-col>
+          </v-row>
+
+
+           <v-row>
+            <v-col cols="4">
+              <p>Lingkup Kerja Sama</p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+              <v-textarea
+                class="rounded-0 ml-4"
+                solo
+                name="input-7-4"
+                label="Lingkup kerjasama antara mitra dengan JTK"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+
+           <v-row>
+            <v-col cols="4">
+              <p>Rencana Kegiatan</p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+              <v-textarea
+                class="rounded-0 ml-4"
+                solo
+                name="input-7-4"
+                label="Rencana kegiatan kerjasama antara mitra dengan JTK"
+              ></v-textarea>
+            </v-col>
+          </v-row>
+
+           <v-row>
+            <v-col cols="4">
+              <p>Surat Permohonan Kerjsama</p>
+            </v-col>
+            <v-col
+              cols="12"
+              sm="6"
+              md="6"
+            >
+               <v-file-input
+                  class="rounded-0 ml-4"
+                  label="Upload file permohonan kerja sama"
+                  outlined
+                  dense
+                ></v-file-input>
+            </v-col>
+          </v-row>
+
+        </v-container>
+
+      <v-row>
+        <v-col></v-col>
+        <v-col cols="12" md="auto">
+              <v-btn
+                @click = submitAction
+                dark
+                class="rounded-0 ml-4"
+                color="#19396C"
+              >Kirim</v-btn>
+        </v-col>
+        <v-col col lg="2"></v-col>
+      </v-row>
 
     </div>
     <Footer></Footer>
@@ -18,6 +163,13 @@ export default {
   components: {
     Header,
     Footer
+  },
+  data: () => ({
+  }),
+  methods: {
+    submitAction(){
+
+    }
   }
 }
 </script>
@@ -28,8 +180,16 @@ export default {
   padding: 0;
 }
 
-h3{
+h2{
     text-align: center;
     margin-top: 2%;
+    margin-bottom: 2%;
 }
+
+.partnership-submission{
+  margin-top: 4%;
+  margin-left: 10%;
+  margin-right: 10%;
+}
+
 </style>
